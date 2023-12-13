5acc439da85b5691295db10c94e87cd9d23c6fac
Anju Vilashni <145509298+anjuvilashni@users.noreply.github.com>
2023-12-13 03:15:16 -0500

    upload

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import numpy as np
 import cv2
 import os
 from flask import Flask
-from flask import (
-    render_template,
-    request
-)
+from flask import render_template, request
 
 app = Flask(__name__)
 app_root = os.path.abspath(os.path.dirname(__file__))
 
 app.secret_key = os.urandom(10)
+
 
 @app.route("/")
 def index():
